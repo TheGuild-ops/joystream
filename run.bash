9e231a32f94a134052b48a6c4a8381dbf936dbfb
TheGuild-ops <79193549+TheGuild-ops@users.noreply.github.com>
2021-02-17 17:05:13 +0300

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,10 +1,12 @@
 echo "PLS enter validator Name"
 read name
-
+rm joy-testnet-4.json*
 wget https://github.com/Joystream/joystream/releases/download/v7.5.0/joystream-node-3.3.0-fdb75f5ec-x86_64-linux-gnu.tar.gz
 tar -vxf joystream-node-3.3.0-fdb75f5ec-x86_64-linux-gnu.tar.gz
 wget https://github.com/Joystream/joystream/releases/download/v7.5.0/joy-testnet-4.json
-
+ufw allow 300000
+ufw allow 1200
+ufw allow 1201
 systemctl stop joy
 cat <<EOF > /etc/systemd/system/joy.service
 
@@ -20,7 +22,10 @@ ExecStart=/root/joystream-node \
         --chain joy-testnet-4.json \
         --pruning archive \
         --validator \
-        --name $name
+        --name $name \
+        --port 30000 \
+        --rpc-port 1200 \
+        --ws-port 1201 
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=8192
